ae4d92b0d74753cde6ab1c242c529aa58903ce54
Siddharth Madhavan <siddharth.madhavan@gmail.com>
2018-03-20 00:36:31 +0800

    improved resume

---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+        $(document).ready(function(){
+            $('.scrollspy').scrollSpy();
+        });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
